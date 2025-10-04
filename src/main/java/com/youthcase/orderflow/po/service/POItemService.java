efe6919985cf3de1b0d5088981dd7ab9012c22f1
jiwon <wlrnejddl@naver.com>
2025-10-05 00:04:20 +0900

    Add PO

--- src/main/java/com/youthcase/orderflow/po/service/POItemService.java
+++ src/main/java/com/youthcase/orderflow/po/service/POItemService.java
@@ -1,12 +1,14 @@
 package com.youthcase.orderflow.po.service;
 
 import com.youthcase.orderflow.po.domain.POItem;
+import com.youthcase.orderflow.po.domain.Status;
+
 import java.util.List;
 
 public interface POItemService {
 
-    // 장바구니 전체 상품 조회
-    List<POItem> getAllItems();
+    // 장바구니 상품 조회: 헤더id와 상태 기준으로 아이템 목록 찾아오기
+    List<POItem> getAllItems(Long poId);
 
     // 상품 수량 변경
     POItem updateItemQuantity(Long gtin, int quantity);
@@ -15,5 +17,5 @@ public interface POItemService {
     void deleteItem(List<Long> gtins);
 
     // 전체 상품 삭제
-    void clearCart();
+    void clearItem();
 }
--- src/main/java/com/youthcase/orderflow/po/service/POItemServiceImpl.java
+++ src/main/java/com/youthcase/orderflow/po/service/POItemServiceImpl.java
@@ -1,8 +1,10 @@
 package com.youthcase.orderflow.po.service;
 
+import com.youthcase.orderflow.po.domain.POHeader;
 import com.youthcase.orderflow.po.domain.POItem;
 import com.youthcase.orderflow.po.domain.Status;
 import com.youthcase.orderflow.po.repository.POItemRepository;
+import com.youthcase.orderflow.pr.domain.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,51 +17,53 @@ import java.util.List;
 public class POItemServiceImpl implements POItemService {
 
     private final POItemRepository poItemRepository;
+    private static final Status statusPr = Status.PR;   // 장바구니 상태
+    private static final Status statusD = Status.D;     // 삭제 상태
+    private static final Status statusGI = Status.GI;        // 출고 처리 상태
 
-    private static final Status statusPr = "PR";   // 장바구니 상태
-    private static final String statusD = "D"; // 삭제 상태
-    private static final String statusGI = "GI"; // 출고 처리 상태
-
-    // 장바구니 전체 상품 조회 (status = PR)
+    // 장바구니 상품 조회
     @Override
-    public List<POItem> getAllItems() {
-        return poItemRepository.findAllByStatus(Status.PR);
+    public List<POItem> getAllItems(Long poId) {
+        return poItemRepository.findByPoHeader_PoIdAndStatus(poId, Status.PR);
     }
 
     // 상품 수량 변경
     @Override
     public POItem updateItemQuantity(Long gtin, int quantity) {
-        POItem item = poItemRepository.findByGtinAndStatus(gtin, statusPr)
-                .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다. GTIN: " + gtin));
+        POItem item = poItemRepository.findByGtinAndStatus(gtin, statusPr).orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다. GTIN: " + gtin));
 
         if (quantity < 1) {
             throw new IllegalArgumentException("수량은 1개 이상이어야 합니다.");
         }
 
         item.setOrderQty((long) quantity);
-        return poItemRepository.save(item);
+        return poItemRepository.save(item);   // save() : 쿼리에서 update 로 작동한다.
     }
 
-    // 선택 상품 삭제 (status = D 로 변경)
+    // 선택 상품 삭제
     @Override
+    @Transactional
     public void deleteItem(List<Long> gtins) {
-        for (Long gtin : gtins) {
-            POItem item = poItemRepository.findByGtinAndStatus(gtin, statusPr)   //pr 일때
-                    .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다. GTIN: " + gtin));
-            item.setStatus(Status.D);
-            poItemRepository.save(item);
-        }
+        List<POItem> items = poItemRepository.findAllByGtinIn(gtins);
+        poItemRepository.deleteAll(items);
     }
 
-    // 전체 상품 삭제 (장바구니 status = PR → D 로 일괄 변경)
+
+
+
+//    장바구니 전체를 삭제할 때, 전체 삭제 버튼이 있나? 음.. 아니야
+//    전체 선택 했을 때만 논리적 삭제를 한다? 뭔가 이상한데..
+//
+//    저장 버튼을 아예 따로 만들까? 저장 버튼..  그게 훨씬 현실적이긴 하지..
+
+    // 전체 상품 삭제 (status = PR → D 로 변경)
     @Override
-    public void clearCart() {
+    public void clearItem() {
         List<POItem> cartItems = poItemRepository.findAllByStatus(Status.PR);
         for (POItem item : cartItems) {
             item.setStatus(Status.D);
         }
         poItemRepository.saveAll(cartItems);
     }
-
 
 }
