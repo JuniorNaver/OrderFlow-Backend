58e1df9a7fec6ea86ba4ff866764c8d26427c667
jigudeong-i <wlrnejddl@naver.com>
2025-10-10 00:47:41 +0900

    request, response 추가

--- src/main/java/com/youthcase/orderflow/po/controller/POController.java
+++ src/main/java/com/youthcase/orderflow/po/controller/POController.java
@@ -1,8 +1,8 @@
 package com.youthcase.orderflow.po.controller;
 
-import com.youthcase.orderflow.po.domain.PO;
-import com.youthcase.orderflow.po.domain.POItem;
 import com.youthcase.orderflow.po.domain.Status;
+import com.youthcase.orderflow.po.dto.POItemRequestDTO;
+import com.youthcase.orderflow.po.dto.POItemResponseDTO;
 import com.youthcase.orderflow.po.service.POHeaderService;
 import com.youthcase.orderflow.po.service.POItemService;
 import com.youthcase.orderflow.po.service.POService;
@@ -26,14 +26,14 @@ public class POController {
 
     /** 장바구니 상품 조회: 헤더id와 상태 기준으로 아이템 목록 찾아오기 */
     @GetMapping("/items")
-    public List<POItem> getAllItems(Long poId, Status status) {     //프론트에서 /items?poId=1&status=PR 이런식으로 요청
+    public List<POItemResponseDTO> getAllItems(Long poId, Status status) {
         return poItemService.getAllItems(poId, status);
     }
 
     /** 장바구니 상품 수량 변경 */
-    @PutMapping("/items/{itemId}")
-    public POItem updateItemQuantity(@PathVariable Long itemId, @RequestBody POItem item) {
-        return poItemService.updateItemQuantity(itemId, item.getOrderQty());
+    @PutMapping("/items/{itemNo}")
+    public POItemResponseDTO updateItemQuantity(@PathVariable Long itemNo, @RequestBody POItemRequestDTO dto) {
+        return poItemService.updateItemQuantity(itemNo, dto);
     }
 
     /** 장바구니 상품 삭제  */
@@ -44,8 +44,8 @@ public class POController {
 
     /** 장바구니 저장 */
     @PostMapping("/save/{poId}")
-    public void saveItem(@PathVariable Long poId){
-        poItemService.saveItem(poId);
+    public void saveItem(@PathVariable Long poId, List<POItemRequestDTO> requestDTOList){
+        poItemService.saveItem(poId, requestDTOList);
     }
 
     /** 발주 확정  */
