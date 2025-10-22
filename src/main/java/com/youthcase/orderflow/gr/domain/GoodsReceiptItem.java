ddf81609c315a45006d95ede7f631551f15922e5
JuniorNaver <withjunhee@gmail.com>
2025-10-22 15:18:16 +0900

    GR 에러 수정, LOT와 ITEM 연관관계 수정

--- src/main/java/com/youthcase/orderflow/gr/domain/GoodsReceiptItem.java
+++ src/main/java/com/youthcase/orderflow/gr/domain/GoodsReceiptItem.java
@@ -43,5 +43,7 @@ public class GoodsReceiptItem {
     private Product product;
 
     @OneToMany(mappedBy = "goodsReceiptItem", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
     private List<Lot> lots = new ArrayList<>();
+
 }
