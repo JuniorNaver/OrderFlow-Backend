ee3d1f0b7ff46b809761d4aeab5532dca07f1315
jigudeong-i <wlrnejddl@naver.com>
2025-10-22 23:27:50 +0900

    장바구니 화면 띄우기

--- src/main/java/com/youthcase/orderflow/po/service/POItemService.java
+++ src/main/java/com/youthcase/orderflow/po/service/POItemService.java
@@ -14,7 +14,7 @@ public interface POItemService {
     POItemResponseDTO addPOItem(Long poId, POItemRequestDTO poItemRequestDTO, String gtin);
 
     /** 장바구니 상품 조회 (poId, status 기준) */
-    List<POItemResponseDTO> getAllItems(Long poId, POStatus status);
+    List<POItemResponseDTO> getAllItems(Long poId);
 
     /** 상품 수량 변경 */
     POItemResponseDTO updateItemQuantity(Long itemNo, POItemRequestDTO requestDTO);
